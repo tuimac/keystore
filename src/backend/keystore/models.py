6341b256f05a72f247f5ff9af487b63aaa20c9dd
tuimac <tuimac.devadm01@gmail.com>
2021-11-14 07:20:57 +0000

    done

--- src/backend/keystore/models.py
+++ src/backend/keystore/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 class Keystore(models.Model):
-    keyname = models.CharField(max_length=255)
+    keyname = models.CharField(max_length=255, primary_key=True)
     username = models.CharField(max_length=255)
     memo = models.TextField()
     genre = models.CharField(max_length=255)
     class Meta:
+        unique_together = (("username", "keyname"),)
         db_table = 'k_keystore'
         managed = False
--- src/backend/keystore/urls.py
+++ src/backend/keystore/urls.py
@@ -3,5 +3,5 @@ from .views import KeystoreBase
 
 urlpatterns = [
     path('', KeystoreBase.as_view()),
-    path('<str:name>/', KeystoreBase.as_view())
+    path('<str:keyname>/', KeystoreBase.as_view())
 ]
