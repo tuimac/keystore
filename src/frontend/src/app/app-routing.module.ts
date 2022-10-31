18e5c668d37169374e1948328dd5514cab979269
tuimac <tuimac.devadm01@gmail.com>
2022-11-01 00:16:52 +0900

    save

--- src/frontend/src/app/app-routing.module.ts
+++ src/frontend/src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { NotFoundComponent } from './utils/notfound/notfound.component';
 import { HomeComponent } from './components/home/home.component';
+import { CreatekeysComponent } from './components/createkeys/createkeys.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'create',
+    component: CreatekeysComponent
   },
   {
     path: '**',
